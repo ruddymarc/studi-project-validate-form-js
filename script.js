a0bba84a2fac239b6364e1a5a23a67fae605d6b7
ROZAS Ruddy <Ruddy@imac-de-ruddy.gp>
2022-07-20 10:14:39 -0400

    ft#03: ajout validation des champs a la saisi (js)

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 (() => {
   const constraintsText = {
-    name: 'name.helper',
-    email: 'email.helper',
-    phoneNumber: 'phoneNumber.helper',
-    category: 'category.helper',
-    message: 'message.helper',
+    name: 'Le nom d\'un utilisateur ne peut contenir plus de 30 caractères et ne doit contenir que des lettres.',
+    email: 'Il doit s\'agir d\'un e-mail valide.',
+    phoneNumber: 'Il doit s\'agir d\'un numéro de téléphone européen.',
+    category: 'Veuillez choisir une catégorie s\'il-vous plais.',
+    message: 'Le message ne peut excéder plus de 200 caractères.',
+  }
+  const constraintsRegex = {
+    name: /^[a-zA-Z-éèëï]{3,30}$/,          // Acept : Gérémie, Noël, Jean-petit
+    email: /^[\w.-]+@([\w-]+\.)[\w]{2,3}$/, // Acept : contact.service@domain.com
+    phoneNumber: /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/,
+    category: /^[a-z-]+$/                   // Acept : snake-format
   }
   // DOM Elements
   const form = document.getElementById('contact-form')
@@ -25,6 +31,15 @@
       })
       input.addEventListener('blur', () => {
         helper.classList.remove('fade')
+      })
+      // Remove helper when fields is valid
+      input.addEventListener('input', () => {
+        if ('message' === input.name) {
+          const maxLength = 200
+          helper.innerText = !(input.value.length < maxLength) ? constraintsText[input.name] : ''
+        } else {
+          helper.innerText = !constraintsRegex[input.name].test(input.value) ? constraintsText[input.name] : ''
+        }
       })
     }
   })
